4362d9c871106145ea767fdaeae8c650ed845b58
Ilya Borovitinov <icehaunter@gmail.com>
2020-09-25 00:14:51 +0300

    Added full config for NPM publishing

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,21 +1,39 @@
 import vue from 'rollup-plugin-vue'
 import typescript from 'rollup-plugin-typescript2'
 import css from 'rollup-plugin-postcss'
+import path from 'path'
 
-export default {
-  input: 'src/components/Datepicker.vue',
-  output: {
-    format: 'esm',
-    file: 'dist/Datepicker.js'
+export default [
+  {
+    output: {
+      format: 'esm',
+      file: 'dist/vue3-datepicker.esm.js',
+    },
+    plugins: [
+      typescript(),
+      vue({ css: false }),
+      css({
+        extract: path.resolve('dist', 'vue3-datepicker.css'),
+      }),
+    ],
   },
+  {
+    output: {
+      format: 'cjs',
+      file: 'dist/vue3-datepicker.cjs.js',
+      exports: 'default'
+    },
+    plugins: [
+      typescript(),
+      vue({ css: false }),
+      css({
+        extract: false,
+        inject: false
+      })
+    ],
+  },
+].map((v) => ({
+  ...v,
+  input: 'src/datepicker/Datepicker.vue',
   external: ['vue', 'date-fns', 'date-fns/fp'],
-  plugins: [
-    typescript(),
-    vue({ css: false }),
-    css({
-      extract: true,
-      minimize: true
-    }),
-    // resolve(),
-  ]
-}
+}))
