1e8e8e9f9b27de0963af9c4fb5f70a3b96b54370
Ilya Borovitinov <icehaunter@gmail.com>
2021-01-22 21:29:58 +0300

    docs: Fixed base to use with gh-pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   title: 'Vue 3 Datepicker',
   description: 'Datepicker component suitable for Vue 3',
+  base: '/vue3-datepicker/',
+
   themeConfig: {
     repo: 'icehaunter/vue3-datepicker',
     docsDir: 'docs',
